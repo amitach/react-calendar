2273d31d1696670abda98501d6397a05f9d1e637
Amit Acharya <amitach@gmail.com>
2017-02-05 17:42:06 +0530

    Post to create but no update

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -9,12 +9,17 @@ var Appointments = React.createClass({
     handleUserInput: function (obj) {
         this.setState(obj);
     },
+    handleFormSubmit: function () {
+        var appointment = {title: this.state.title, appointment_time: this.state.appointment_time}
+        $.post('/appointments', {appointment: appointment});
+    },
     render: function () {
         return (
             <div>
                 <AppointmentForm input_title={this.state.title}
                                  input_appointment_time={this.state.appointment_time}
                                  onUserInput={this.handleUserInput}
+                                 onFormSubmit={this.handleFormSubmit}
                 />
                 <AppointmentsList appointments={this.state.appointments} />
             </div>
